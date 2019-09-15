ab19f514c7c7290aef8a83f118a6b7e19844e003
Vlad Grytsenko <vladik.grytsenko2017@gmail.com>
2019-09-15 22:16:37 +0300

    Updates

---
+++ precache-manifest.5904b1b3169ea40a8d2056710753f609.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f889f1a3851b187b64f34161d26e1f67",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "2b7bceaa191439febf5b",
+    "url": "/react_tabs/static/css/main.0ae14f24.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/react_tabs/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "2b7bceaa191439febf5b",
+    "url": "/react_tabs/static/js/main.7e548171.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
